955e4301b2ce38b67f4418f6fb55dec40b6038a3
linnnruoo <linruo2014@gmail.com>
2019-10-19 09:26:21 -0700

    add launching

---
+++ SpaceShipMuseum/MainscreenViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainscreenViewController.swift
+//  NASA_Hackathon
+//
+//  Created by Lynn Bao on 10/19/19.
+//  Copyright © 2019 Brian Advent. All rights reserved.
+//
+
+
+import UIKit
+
+class MainscreenViewController: UIViewController {
+    
+    @IBOutlet weak var buttonProps: UIButton!
+    
+    override func viewDidLoad() {
+        buttonProps.contentCompressionResistancePriority(for: <#T##NSLayoutConstraint.Axis#>)
+    }
+    
+}
